ba937640ecc9c058d80142bb4377550fe800f986
bg-write <bradywgerber@gmail.com>
2021-02-03 18:43:00 -0500

    Create Writer model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Writer(models.Model):
+    name = models.CharField(max_length=100)
+    birth = models.IntegerField()
+    death = models.IntegerField()
+    works = models.TextField(max_length=250)
+    quote = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.name
