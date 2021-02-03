8ab275da070b813243c2e69d69f8d45b6fc63ad5
bg-write <bradywgerber@gmail.com>
2021-02-03 14:19:19 -0500

    Stub up auth logic

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def home(request):
   return HttpResponse('<h1>Sanity</h1>')
@@ -7,4 +11,16 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
-
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up, please try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
