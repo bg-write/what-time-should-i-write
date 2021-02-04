684217f71c5a94f25487daec7c954497c4b463a9
bg-write <bradywgerber@gmail.com>
2021-02-03 19:33:04 -0500

    Update Writer model

--- main_app/models.py
+++ main_app/models.py
@@ -4,8 +4,8 @@ class Writer(models.Model):
     name = models.CharField(max_length=100)
     birth = models.IntegerField()
     death = models.IntegerField()
-    works = models.TextField(max_length=250)
-    quote = models.TextField(max_length=500)
+    works = models.TextField(max_length=500)
+    quote = models.TextField(max_length=1000)
 
     def __str__(self):
         return self.name
