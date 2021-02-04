6f33171688bbc2f1d3a4c1af28c790a12d1be110
bg-write <bradywgerber@gmail.com>
2021-02-03 19:03:34 -0500

    Import admin access

--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Writer
 from django.http import HttpResponse
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-
-class Writer:
-  def __init__(self, name, birth, death, works, quote):
-    self.name = name
-    self.birth = birth
-    self.death = death
-    self.works = works
-    self.quote = quote
-
-writers = [
-  Writer('Writer A', 1925, 2000, 'Notable Work 1', 'This is quote A.'),
-  Writer('Writer B', 1935, 2010, 'Notable Work 2', 'This is quote B.'),
-  Writer('Writer C', 1940, 1999, 'Notable Work 3', 'This is quote C.'),
-]
 
 def home(request):
   return render(request, 'home.html')
@@ -26,6 +13,7 @@ def about(request):
   return render(request, 'about.html')
 
 def writers_index(request):
+  writers = Writer.objects.all()
   return render(request, 'writers/index.html', { 'writers': writers })
 
 
