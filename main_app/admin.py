6996fece48a89ba483cf8a84f3b5abbdc07635b5
bg-write <bradywgerber@gmail.com>
2021-02-05 12:51:58 -0500

    Add ability to add photos

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Writer, Routine
+from .models import Writer, Routine, Photo
 
 admin.site.register(Writer)
 admin.site.register(Routine)
+admin.site.register(Photo)
--- main_app/views.py
+++ main_app/views.py
@@ -8,6 +8,12 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView
 from .models import Writer
 from .forms import RoutineForm
+import uuid
+import boto3
+from .models import Writer, Photo
+S3_BASE_URL = 'https://s3.us-east-1.amazonaws.com/'
+BUCKET = 'writerroutinebg'
+
 
 def home(request):
   return render(request, 'home.html')
@@ -54,3 +60,17 @@ def signup(request):
 class WriterCreate(CreateView):
   model = Writer
   fields = '__all__'
+
+def add_photo(request, writer_id):
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+      s3 = boto3.client('s3')
+      key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+      try:
+          s3.upload_fileobj(photo_file, BUCKET, key)
+          url = f"{S3_BASE_URL}{BUCKET}/{key}"
+          photo = Photo(url=url, writer_id=writer_id)
+          photo.save()
+      except:
+          print('An error occurred uploading file to S3')
+  return redirect('detail', writer_id=writer_id)
