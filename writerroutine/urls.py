54054a7726ee645333382396b3ca34a85d9e07f0
bg-write <bradywgerber@gmail.com>
2021-02-06 10:18:16 -0500

    Add favicon

--- writerroutine/urls.py
+++ writerroutine/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_app.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico')),),
 ]
