69b9dade5f22759689289a6db12cc75c3eb2b48e
bg-write <bradywgerber@gmail.com>
2021-02-06 14:25:40 -0500

    Update writer model, take off quote char limit

--- main_app/models.py
+++ main_app/models.py
@@ -6,8 +6,8 @@ class Writer(models.Model):
     name = models.CharField(max_length=100)
     birth = models.IntegerField()
     death = models.IntegerField()
-    works = models.TextField(max_length=500)
-    quote = models.TextField(max_length=1000)
+    works = models.TextField()
+    quote = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
